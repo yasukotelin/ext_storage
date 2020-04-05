7bd679688fb48ad6eafc8641a8abc1ce17ad18e6
yasukotelin <31115673+yasukotelin@users.noreply.github.com>
2020-04-05 17:25:36 +0900

    Update the example simply

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,124 +1,54 @@
 import 'package:ext_storage/ext_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ExtStorage Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'ExtStorage Demo'),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  String _dropdownValue;
-  String _path;
-  bool _isShowProgressing;
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
 
-  List<DropdownMenuItem> _functionItems = <String>[
-    'getApplicationDocumentsDirectory()',
-    'getLibraryDirectory()',
-    'getApplicationSupportDirectory()',
-    'getTemporaryDirectory()',
-    'getExternalStorageDirectory()',
-    'ExtStorage.getExternalStorageDirectory()',
-    'ExtStorage.getExternalStoragePublicDirectory(ExtStorage.DIRECTORY_PICTURES)',
-  ].map<DropdownMenuItem<String>>((String value) {
-    return DropdownMenuItem<String>(
-      value: value,
-      child: Text(value),
-    );
-  }).toList();
+  final String title;
 
   @override
-  void initState() {
-    super.initState();
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-    _path = "";
-    _isShowProgressing = false;
+class _MyHomePageState extends State<MyHomePage> {
+  Future<String> _getPath() {
+    return ExtStorage.getExternalStoragePublicDirectory(
+        ExtStorage.DIRECTORY_PICTURES);
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'exampl',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("example"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 300.0,
-                child: DropdownButton<String>(
-                  isExpanded: true,
-                  value: _dropdownValue,
-                  hint: Text('Select the function you want to try.'),
-                  elevation: 16,
-                  style: TextStyle(color: Colors.deepPurple),
-                  underline: Container(
-                    height: 2,
-                    color: Colors.deepPurpleAccent,
-                  ),
-                  onChanged: (String newValue) {
-                    setState(() {
-                      _dropdownValue = newValue;
-                    });
-                  },
-                  items: _functionItems,
-                ),
-              ),
-              Padding(padding: EdgeInsets.only(top: 20)),
-              RaisedButton(
-                child: Text("Get path!"),
-                onPressed: () {
-                  onPush();
-                },
-              ),
-              Padding(padding: EdgeInsets.only(top: 40.0)),
-              _isShowProgressing ? CircularProgressIndicator() : Text(_path),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: FutureBuilder(
+          future: _getPath(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data);
+            } else {
+              return Text("Loading");
+            }
+          },
         ),
       ),
     );
-  }
-
-  void onPush() async {
-    setState(() {
-      _isShowProgressing = true;
-    });
-    switch (_dropdownValue) {
-      case "getApplicationDocumentsDirectory()":
-        _path = (await getApplicationDocumentsDirectory()).path;
-        break;
-      case "getLibraryDirectory()":
-        _path = (await getLibraryDirectory()).path;
-        break;
-      case "getApplicationSupportDirectory()":
-        _path = (await getApplicationSupportDirectory()).path;
-        break;
-      case "getTemporaryDirectory()":
-        _path = (await getTemporaryDirectory()).path;
-        break;
-      case "getExternalStorageDirectory()":
-        _path = (await getExternalStorageDirectory()).path;
-        break;
-      case "ExtStorage.getExternalStorageDirectory()":
-        _path = await ExtStorage.getExternalStorageDirectory();
-        break;
-      case "ExtStorage.getExternalStoragePublicDirectory(ExtStorage.DIRECTORY_PICTURES)":
-        _path = await ExtStorage.getExternalStoragePublicDirectory(
-            ExtStorage.DIRECTORY_PICTURES);
-        print(_path);
-        break;
-      default:
-        _path = "";
-        break;
-    }
-    setState(() {
-      _isShowProgressing = false;
-    });
   }
 }
